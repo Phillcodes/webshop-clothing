2cfce737127da58589b5bf1c01c3db08af8998df
phillcodes <phillipjacob90@gmail.com>
2020-04-13 14:01:09 +0200

    updated routes using withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,13 +12,16 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
                   linkUrl: 'shop/hats'
+                
                 },
+                
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
                   linkUrl: 'shop/jackets'
                 },
+                
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
@@ -39,7 +42,7 @@ class Directory extends React.Component {
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/mens'
-                }
+                },
               ]
         }
 
@@ -50,8 +53,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
         {
-    this.state.sections.map(({title, imageUrl, id, size}) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+    this.state.sections.map(({ id, ...otherSectionProps}) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))
         }       
 
@@ -60,6 +63,9 @@ class Directory extends React.Component {
     }
 }
 
+
+// this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+ // <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
 
 
 export default Directory;
