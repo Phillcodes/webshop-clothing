f067c714632afd57d0153015e6f365b5d8d6875f
Phillipjacob <phillipjacob90@gmail.com>
2020-05-04 20:29:12 +0200

    fixed some google auth mistyping and ran prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import HomePage from './pages/homepage/homepage.component.jsx';
-import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx';
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.jsx";
+import { auth } from "./firebase/firebase.utils";
 
-import './App.css';
+import "./App.css";
 
+class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
 
-   <Route exact path='/' component={HomePage} />
-   <Route path='/shop' component={ShopPage} />
-   <Route path='/signin' component={SignInAndSignUpPage} />
- </Switch>
-
-    </div>
-  );
+      console.log(user);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
